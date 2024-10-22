63b86cfac94afeb2e1e9cdb9dc07d977607f9914
matwiniarczyk <matiwiniar@gmail.com>
2024-10-22 21:29:53 +0200

    Dodanie formularza kontaktowego

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
+
+from .forms import ContactForm
 from .models import ProjectsDone
 
 
 class ProjectsDoneSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectsDone
+        fields = '__all__'
+
+
+class ContactFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactForm
         fields = '__all__'
